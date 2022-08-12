f4a384a38c2acd7c69480a086293bc7f22d60664
Yuvraj <mryuvrajsbhadauria@gmail.com>
2022-08-12 19:20:09 +0530

    Splash screen added

--- app/src/main/java/com/yuvraj/cab_booking_app/MainActivity.kt
+++ app/src/main/java/com/yuvraj/cab_booking_app/MainActivity.kt
@@ -4,12 +4,14 @@ package com.yuvraj.cab_booking_app
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.google.android.gms.auth.api.Auth
 import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContentView(R.layout.activity_main)
     }
     override fun onStart() {
